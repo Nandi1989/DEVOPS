95714cf773343127281377f7a546ff4cc0e49a96
Nandi1989 <nandi.hc@gmail.com>
2021-06-05 21:18:51 +0200

    Makefile updated for help command

--- Roboshop/components/Frontend.sh
+++ Roboshop/components/Frontend.sh
@@ -3,11 +3,28 @@
 source components/Common.sh
 
 HEAD "Install nginx"
-yum install nginx -y
+yum install nginx -y &>>/tmp/roboshop.log
 STAT $?
 
-HEAD "Start nginx"
-systemctl enable nginx
-systemctl start nginx
+HEAD "Download HTDOCS content from GITHUB"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>/tmp/roboshop.log
 STAT $?
 
+HEAD "Remove html files "
+rm -rf /usr/share/nginx/html/*
+STAT $?
+
+HEAD "Extract HTDOCS content"
+unzip -d /usr/share/nginx/html/ /tmp/frontend.zip &>>/tmp/roboshop.log
+mv /usr/share/nginx/html/frontend-main/* /usr/share/nginx/html/. &>>/tmp/roboshop.log
+mv /usr/share/nginx/html/static/* /usr/share/nginx/html/. &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Move conf file"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+STAT $?
+
+HEAD "Restart nginx"
+systemctl enable nginx &>>/tmp/roboshop.log
+systemctl restart nginx &>>/tmp/roboshop.log
+STAT $?
