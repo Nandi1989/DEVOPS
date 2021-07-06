e81e0640693a52564d30880fbc7ad844900a7b04
Nandi1989 <nandi.hc@gmail.com>
2021-07-06 21:41:06 +0200

    echo line updated

--- Roboshop/instance_launch/launch.sh
+++ Roboshop/instance_launch/launch.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-component=$1
+COMPONENT=$1
 
 ## component name check
-if [ -z ${component} ];  then
+if [ -z ${COMPONENT} ];  then
     echo -e "\e[31m component name is needed \e[0m"
     exit 1
 fi
 
-##LTID=lt-0bc9e6d40bf1e5c3c
-##LTIDVER=2
+LTID=lt-0bc9e6d40bf1e5c3c
+LTIDVER=2
 
-##INSTANCE_CREATE() {
-##  aws ec2 describe-instances --filters "name=tag:Name,values=${component}"
+INSTANCE_CREATE() {
+  aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"
 
 
 ##}
