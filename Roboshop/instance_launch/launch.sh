f1b2980e9307354465f91809d076fc02101ad219
Nandi1989 <nandi.hc@gmail.com>
2021-07-06 22:04:32 +0200

    instance run command updated

--- Roboshop/instance_launch/launch.sh
+++ Roboshop/instance_launch/launch.sh
@@ -11,12 +11,12 @@ fi
 LID=lt-0bc9e6d40bf1e5c3c
 LVER=2
 
-INSTANCE_CREATE() {
+##INSTANCE_CREATE() {
 ##  aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"
   aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"
 
 
 
-}
+##}
 
 ##if
