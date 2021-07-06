62704c2969575f5fc4397867965c824882bb0e18
Nandi1989 <nandi.hc@gmail.com>
2021-07-06 21:44:31 +0200

    instance run command added

--- Roboshop/instance_launch/launch.sh
+++ Roboshop/instance_launch/launch.sh
@@ -8,8 +8,8 @@ if [ -z ${COMPONENT} ];  then
     exit 1
 fi
 
-LTID=lt-0bc9e6d40bf1e5c3c
-LTIDVER=2
+LID=lt-0bc9e6d40bf1e5c3c
+LVER=2
 
 INSTANCE_CREATE() {
   aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"
