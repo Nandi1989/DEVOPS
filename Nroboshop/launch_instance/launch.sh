19e642862c615ece504df194188e659a42e953a1
Nandi1989 <nandi.hc@gmail.com>
2021-07-18 12:03:23 +0200

    updated

--- Nroboshop/launch_instance/launch.sh
+++ Nroboshop/launch_instance/launch.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
+COMPONENT=$1
+
+if [ -z ${COMPONENT} ];then
+  echo "value of COMPONENT IS empty"
+  exit
+fi
+
+LID=i-04f089715ed156371
+LVER=2
+
+INSTANCE_CREATE() {
+ INSTANCE_STATE=$(aws ec2 describe-instnaces --filters "name=tag:name,values=${COMPONENT}"--sg-09436406187809d28
 echo "Instances"
-describe instances
+
+
+}
--- Roboshop/instance_launch/launch.sh
+++ Roboshop/instance_launch/launch.sh
@@ -8,15 +8,35 @@ if [ -z ${COMPONENT} ];  then
     exit 1
 fi
 
-LID=lt-0bc9e6d40bf1e5c3c
-LVER=2
+LID=lt-0bae2d44a42501107
+LVER=1
 
+
+DNS_UPDATE() {
+  PRIVATEIP=$(aws ec2 describe-instances --filters "Name=tag:Name, Values:${COMPONENT}" | jq .Reservations[].Instances[].PrivateIpAddress | xargs n1)
+  sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" record.json > tmp/record.json
+  aws route53 change-resource-record-sets --hosted-zone-id Z031396221895VDGBP6VC --change-batch file:///tmp/record.json | jq
+
+}
 
 INSTANCE_CREATE() {
-aws ec2 desc
+  INSTANCE_STATE=$(aws ec2 describe-instances --filters "Name=tag:Name, Values:${COMPONENT}" | jq .Reservations[].Instances[].State.Name | xargs n1)
 
+  if [ ${INSTANCE_STATE} == "running" ]; then
+    echo "Instance already exists"
+    DNS_UPDATE
+    return 0
+  fi
 
-  aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"
+  if [ ${INSTANCE_STATE} == "stopped" ]; then
+    echo "Instance already exists"
+    return 0
+  fi
+
+  echo -n Instance ${COMPONENT} created - IPADDRESS is
+  aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq | grep  PrivateIpAddress  |xargs -n1
+  sleep 30
+  DNS_UPDATE
 
 }
 
